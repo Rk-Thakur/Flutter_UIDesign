b66734568178d4ea2ddf1c933651d341c12de7d4
Rk-Thakur <tranjan638@gmail.com>
2023-06-20 00:28:25 +0545

    EdTech UI Started

--- lib/splash/view/splash_screen.dart
+++ lib/splash/view/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,8 +11,37 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.red,
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 44.h,
+          ),
+          SizedBox(
+            width: 375.w,
+            height: 326.h,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'illustration.png',
+                  width: 375.w,
+                  height: 264.h,
+                ),
+                const Spacer(),
+                Text(
+                  "CodeFactory",
+                  style: TextStyle(
+                    fontSize: 40.sp,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
